b168e6d4ba43ea0ff084273b55b07ab09b112f45
Zaid Khan <zaidiiitk26@gmail.com>
2025-07-26 03:05:32 +0530

    ApiError, ApiResponse, AsyncHandler in utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runnig at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed", error);
+  });
+
+
 
 
 
