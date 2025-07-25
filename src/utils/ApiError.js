e60306b0add7b855cc183510cb7731f9aad4a796
Zaid Khan <zaidiiitk26@gmail.com>
2025-07-26 04:37:06 +0530

    add video and user model

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,7 +8,7 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.data = null;
-    this.messag = message;
+    this.message = message;
     this.success = false;
     this.errors = errors;
 
