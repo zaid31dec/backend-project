33a4b789f78d12c327d750d70541ad8b1e654584
Zaid Khan <zaidiiitk26@gmail.com>
2025-07-27 06:14:23 +0530

    router configured

--- src/app.js
+++ src/app.js
@@ -14,6 +14,15 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(express.cookieParser());
+app.use(cookieParser());
 
+
+// router import
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use('/api/v1/users',userRouter)
+
+// http://localhost:8000/api/v1/users/register
 export { app };
